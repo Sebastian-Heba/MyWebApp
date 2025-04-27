c69201f1cfe479ae8ddee4859a0ad24843f96904
Sebastian-Heba <sebastian.heba86@gmail.com>
2025-04-27 21:15:33 +0200

    dodanie listener'ow

--- webapp.tf
+++ webapp.tf
@@ -301,3 +301,33 @@ resource "aws_lb" "web_app_lb" {
     Name = "Web-App-LoadBalancer"
   }
 }
+# Listener dla HTTPS (port 443)
+resource "aws_lb_listener" "https_listener" {
+  load_balancer_arn = aws_lb.web_app_lb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:<region>:<account-id>:certificate/<certificate-id>"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.web_app_tg.arn
+  }
+}
+
+
+# Listener dla HTTP (port 80) - do przekierowania na HTTPS
+resource "aws_lb_listener" "http_redirect_listener" {
+  load_balancer_arn = aws_lb.web_app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      protocol = "HTTPS"
+      port     = "443"
+      status_code = "HTTP_301"
+    }
+  }
+}
